183ef7ef4185e03ee49a437509945be423f23420
Severin Klingler <severin.klingler@outlook.com>
2020-03-31 20:49:01 +0100

    Cleanup MCP3008 code.

--- python/greenpi/hardware/MCP3008.py
+++ python/greenpi/hardware/MCP3008.py
@@ -1,4 +1,4 @@
-from spidev import SpiDev
+import spidev
 
 # Interface to the MCP3008
 #
@@ -6,11 +6,15 @@ from spidev import SpiDev
 # See documentation/sensors/MCP3008.pdf for information about the sensor.
 
 class MCP3008:
+    START_BIT = 1
+    DONT_CARE = 0 # byte ignored by the protocol
+    SINGLE_ENDED = 8 # Communication type 
+
     def __init__(self, bus = 0, device = 0):
         self.bus = bus
         self.device = device
         self.default_clock_frequency = 1350000
-        self.spi = SpiDev()
+        self.spi = spidev.SpiDev()
         self.open()
  
     def open(self):
@@ -18,7 +22,9 @@ class MCP3008:
         self.spi.max_speed_hz = self.default_clock_frequency
     
     def read(self, channel = 0):
-        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        adc = self.spi.xfer2([self.START_BIT, (self.SINGLE_ENDED | channel) << 4, self.DONT_CARE])
+
+        # we get 10 bits in return (low to bits in first byte and full second byte)
         data = ((adc[1] & 3) << 8) + adc[2]
         return data
             
